c2c84ab7f8e2277f6e18a9e753b21040c014322f
JohnP <hprobotic@gmail.com>
2016-10-20 19:06:58 +0700

    First init

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 #!/usr/bin/env babel-node
 
 require('./helper')
+let fs = require('fs').promise
+let express = require('express')
+let PromiseRouter = require('express-promise-router')
+let morgan = require('morgan')
+let trycatch = require('trycatch')
+let bodyParser = require('body-parser')
 
 async function main() {
-    // Use 'await' in here
-    console.log('main()...')
-    
-    // Your implementation here
+    let app = express()
+    app.use(morgan('dev'))
+
+    app.use((req, res, next) => {
+        trycatch(next, e => {
+            console.log(e.stack)
+            res.writeHead(500)
+            res.end(e.stack)
+        })
+    })
+
+    let router = PromiseRouter()
+    console.log(router)
+    Object.assign(app, router)     // Inherit methods
+    app.use(router)
+
+    let port = 8000
+    await app.listen(port)
+    console.log(`LISTENING @ http://127.0.0.1:${port}`)
 }
 
 main()
